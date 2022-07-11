12a335d265c21d5cd4db9b47a4de813866a4aae4
Elia Manara <elia.manara@yahoo.it>
2022-07-11 18:46:29 +0200

    options

--- 3_argparse.py
+++ 3_argparse.py
@@ -12,14 +12,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "num",
-        help="The Fibonacci number you wish to calculate",
+        help="The Fibonacci number you wish to calculate.",
         type=int
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output result to a file.",
+        action="store_true"
     )
 
     args = parser.parse_args()
 
     result = fib(args.num)
-    print("The {}th Fibonacci number is {}".format(args.num, result))
+    print("The {}th Fibonacci number is {}.".format(args.num, result))
+
+    if (args.output):
+        with open("fibonacci.txt", "a") as f:
+            f.write("The {}th Fibonacci number is {}.\n".format(args.num, result))
 
 
 if __name__ == '__main__':
