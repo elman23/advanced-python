1a8b2180b220854a3f1ab3e20976dd175376b8ff
Elia Manara <elia.manara@yahoo.it>
2022-07-11 20:44:05 +0200

    locks on threads

--- 5_threads.py
+++ 5_threads.py
@@ -1,20 +1,27 @@
-from threading import Thread
+import threading
 import time
+
+
+t_lock = threading.Lock()
 
 
 def timer(name, delay, repeat):
     print("Timer: {} started!".format(name))
+    t_lock.acquire()
+    print("Thread {} acquired the lock!".format(name))
     while repeat > 0:
         time.sleep(delay)
         actual_time = str(time.ctime(time.time()))
         print("{}: {}".format(name, actual_time))
         repeat -= 1
+    print("Thread {} releasing the lock!".format(name))
+    t_lock.release()
     print("Timer: {} completed!".format(name))
 
 
 def main():
-    t1 = Thread(target=timer, args=("Timer1", 1, 5))
-    t2 = Thread(target=timer, args=("Timer2", 2, 3))
+    t1 = threading.Thread(target=timer, args=("Timer1", 1, 5))
+    t2 = threading.Thread(target=timer, args=("Timer2", 2, 3))
     t1.start()
     t2.start()
     print("Function 'main' completed!")
