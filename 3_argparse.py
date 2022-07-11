dd014b0dd9eb003dd05e5c7912faf0a4c159f970
Elia Manara <elia.manara@yahoo.it>
2022-07-11 18:51:00 +0200

    mutually exclusive options

--- 3_argparse.py
+++ 3_argparse.py
@@ -10,6 +10,19 @@ def fib(n):
 
 def main():
     parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbose output.",
+        action="store_true"
+    )
+    group.add_argument(
+        "-q",
+        "--quiet",
+        help="Quiet output.",
+        action="store_true"
+    )
     parser.add_argument(
         "num",
         help="The Fibonacci number you wish to calculate.",
@@ -25,7 +38,13 @@ def main():
     args = parser.parse_args()
 
     result = fib(args.num)
-    print("The {}th Fibonacci number is {}.".format(args.num, result))
+    
+    if args.verbose:
+        print("The {}th Fibonacci number is {}.".format(args.num, result))
+    elif args.quiet:
+        print("Result: {}".format(result))
+    else:
+        print("Fib({}) = {}".format(args.num, result))
 
     if (args.output):
         with open("fibonacci.txt", "a") as f:
